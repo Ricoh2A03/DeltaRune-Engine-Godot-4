7a95360f0ebe7b796dcc70e33e480e91e8632729
David <davidsarkisyanreal@gmail.com>
2025-07-05 22:03:52 +0300

    Globals added, error message implemented

--- Rooms/Base Room/room.gd
+++ Rooms/Base Room/room.gd
@@ -1,5 +1,10 @@
 class_name Room extends Node2D
 
 
+
 @export var music_track: AudioStream
 @export var room_size: Vector2i
+
+
+func initialize_room() -> void:
+	Globals.main
--- main.gd
+++ main.gd
@@ -11,13 +11,35 @@ const TRANSITION_SPEED_LONGEST: float = 2
 @export var first_scene: String
 
 
+var player_instance: Player = null
+var camera_instance: Camera2D = null
+
+
+var _player_scene: String = "uid://dwyq18f0gn5kj"
+var _camera_scene: String = "uid://c7rrg4fvt4xc7"
+
 var _current_scene = null
+var _current_room_size: Vector2i = Vector2i(0, 0)
 
 
 @onready var fade_effect: ColorRect = $FadeEffect
+@onready var music_player = $MusicPlayer
+@onready var error_label = $ErrorLabel
+
+
+func _enter_tree():
+	Globals.main = self
 
 
 func _ready() -> void:
+	if !first_scene:
+		error_label.text = "Oops, you didn't specify your \nstarting scene. " + \
+		"\n\nBetter luck next debugging session." + \
+		"\n\n\n\n\n\n\nP.S.\nAnd don't forget to take a break\nfrom time to time."
+		return
+
+	player_instance = _create_player()
+	camera_instance = _create_camera()
 	change_scene(false, TRANSITION_SPEED_NORMAL, first_scene)
 
 
@@ -37,3 +59,15 @@ func change_scene(fade_in: bool, duration_seconds: float, scene_path: String) ->
 
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(fade_effect, "self_modulate", Color(0.0, 0.0, 0.0, 0.0), duration_seconds)
+
+
+func _create_player() -> Player:
+	var instance = load(_player_scene).instantiate()
+	add_child(instance)
+	return instance
+
+
+func _create_camera() -> Camera2D:
+	var instance = load(_camera_scene).instantiate()
+	add_child(instance)
+	return instance
